9939f52b094d09f849b2716edaf72d059405edee
AhmadRaza1194 <112308719+AhmadRaza1194@users.noreply.github.com>
2024-04-18 15:11:53 +0500

    Back Button

--- app/src/main/java/com/example/semesterproject/LoginPage.java
+++ app/src/main/java/com/example/semesterproject/LoginPage.java
@@ -2,13 +2,24 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
-
+ Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        button1 = findViewById(R.id.sign_up);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this,Sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/semesterproject/Sign_up.java
+++ app/src/main/java/com/example/semesterproject/Sign_up.java
@@ -2,13 +2,24 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Sign_up extends AppCompatActivity {
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        button1 = findViewById(R.id.Back_btn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sign_up.this,LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
